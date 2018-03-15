4f89a3818f700ba21bef9af74207df3b45a2c485
Rene Dohmen (imac) <acidjunk@gmail.com>
2018-03-15 23:35:27 +0100

    Fixed admin for many2many relations

--- shop/app.py
+++ shop/app.py
@@ -53,7 +53,7 @@ app.config['SECURITY_USER_IDENTITY_ATTRIBUTES'] = ['email', 'username']
 db = SQLAlchemy(app)
 db.UUID = UUID
 
-api = Api(app)
+api = Api(app, doc='/api/ui')
 manager = Manager(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
@@ -208,40 +208,8 @@ def before_first_request():
 
 
 class UserAdminView(ModelView):
-    # Don't display the password on the list of Users
-    column_exclude_list = list = ('password',)
-
-    # Don't include the standard password field when creating or editing a User (but see below)
-    form_excluded_columns = ('password',)
-
-    # Automatically display human-readable names for the current and available Roles when creating or editing a User
-    column_auto_select_related = True
-
-    # Prevent administration of Users unless the currently logged-in user has the "admin" role
-    def is_accessible(self):
-        #todo only admins allowed
-        return current_user.is_authenticated
-
-    # On the form for creating or editing a User, don't display a field corresponding to the model's password field.
-    # There are two reasons for this. First, we want to encrypt the password before storing in the database. Second,
-    # we want to use a password field (with the input masked) rather than a regular text field.
-    def scaffold_form(self):
-        # Start with the standard form as provided by Flask-Admin. We've already told Flask-Admin to exclude the
-        # password field from this form.
-        form_class = super(UserAdminView, self).scaffold_form()
-
-        # Add a password field, naming it "password2" and labeling it "New Password".
-        form_class.password2 = PasswordField('New Password')
-        return form_class
-
-    # This callback executes when the user saves changes to a newly-created or edited User -- before the changes are
-    # committed to the database.
-    def on_model_change(self, form, model, is_created):
-        # If the password field isn't blank...
-        if len(model.password2):
-            # ... then encrypt the new password prior to storing it in the database. If the password field is blank,
-            # the existing password in the database will be retained.
-            model.password = utils.hash_password(model.password2)
+    column_hide_backrefs = False
+    column_list = ('email', 'active', 'roles')
 
 
 class RolesAdminView(ModelView):
